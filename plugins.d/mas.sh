2b40dbcd074810d869605fb71216bb352879c081
Mathieu Santostefano <msantostefano@jolicode.com>
2018-10-01 23:33:12 +0200

    🎨,🐛  Make cleanup arg works, remove deprecated cask cleanup

--- plugins.d/mas.sh
+++ plugins.d/mas.sh
@@ -2,13 +2,6 @@
 echo "🍏  Mac App Store updates come fast as lightning"
 # only ask about `mas upgrade` if `mas outdated` returns something
 if [ ! -z "$(mas outdated)" ]; then
-  mas outdated
-  echo ""
-  read -p "Do you wanna run mas upgrade? (y/n)" choice
-  case "$choice" in
-    y|Y ) mas upgrade;;
-    n|N ) echo "Ok, let's continue";;
-    * ) echo "invalid";;
-  esac
+  mas upgrade
 fi
 echo ""
